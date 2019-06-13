c33e7e71ed014258c01cda574be0fbacbfe7c09e
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-13 21:47:39 +0100

    First cut: refactored

--- vars/pipelineChooser.groovy
+++ vars/pipelineChooser.groovy
@@ -1,14 +1,5 @@
 def call(Map config){
 
-	step{
-		config.each{ 
-			log.info "$it.key  : $it.value" 
-		}
-
-		env.each{ 
-			log.info "$it.key  : $it.value" 
-		}
-	}
-	
 	checkoutSource config
+
 }
