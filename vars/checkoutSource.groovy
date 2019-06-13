81fd17abb5700a0bb8e94fc0eb05db4105d79841
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-13 22:01:31 +0100

    First cut: refactored

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -1,6 +1,15 @@
 def call(Map config){
 	agent any
 	stage("CHECKOUT"){
+	
+		config.each{ 
+			log.info "$it.key  : $it.value" 
+		}
+
+		env.each{ 
+			log.info "$it.key  : $it.value" 
+		}
+
 		git url: "git@github.com:krushnatkhawale/cd-lib.git"
 	}
 
