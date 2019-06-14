420b558f06c8812de539e2d7bdbabaa6a6d6b570
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-14 23:36:25 +0100

    Second cut: Ansible playbook integration

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -2,7 +2,8 @@ def call(Map config){
 
 	stage('PRE-PROD DEPLOY'){
 		node{
-			sh 'echo Deploy done'
+			sh 'PRE-PROD deployment started'
+			ansiblePlaybook installation: 'Ansible from jenkins', inventory: 'deployment/inventory/inventory.yml', playbook: 'deployment/playbook/playbook.yml'
 		}
 	}
 	cucumber config
