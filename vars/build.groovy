c7f9cfa7d587dc5ce4a9bbe156fdae29f6e63538
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-16 18:32:40 +0100

    Second cut: static environments added

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,8 +3,10 @@ def call(Map config){
 	stage('BUILD'){
 		node{
 			sh 'chmod +x gradlew'
-			sh './gradlew build'
+			sh './gradlew assemble'
+			config.environment = 'pro-prod'
 		}
 	}
+
 	deployToPreProd config
 }
--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -3,7 +3,13 @@ def call(Map config){
 	stage('PRE-PROD DEPLOY'){
 		node{
 			sh 'echo PRE-PROD deployment started'
-			ansiblePlaybook inventory: 'deployment/inventory/inventory.yml', playbook: 'deployment/playbook/playbook.yml'
+
+			environment = config.environment
+
+			playbook = 'deployment/playbook/playbook.yml'
+			inventory = 'deployment/inventory/${environment}/inventory.yml'
+
+			ansiblePlaybook inventory: inventory, playbook: playbook
 		}
 	}
 	cucumber config
