be645099eda2856bdc5a02c87a00ad724fdd228c
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-16 19:22:13 +0100

    Second cut: static environments added

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -1,11 +1,11 @@
 def call(Map config){
 
-	config.each { echo "echo [INFO] MESSAGE: $it.key : $it.value" }
+	config.each { echo "echo [INFO] MESSAGE :$it.key : $it.value" }
 	stage('PRE-PROD DEPLOY'){
 		node{
 			sh 'echo PRE-PROD deployment started'
 
-			config.each { "echo [INFO] $it.key : $it.value" }
+			config.each { echo "echo [INFO] KRUSH $it.key : $it.value" }
 
 			environment = config.environment
 
