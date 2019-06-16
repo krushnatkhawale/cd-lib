50deb27c07c77bb5c5bd035611fefebd52527eba
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-16 19:56:54 +0100

    Second cut: static environments added

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -2,14 +2,9 @@ def call(Map config){
 
 	stage('PRE-PROD DEPLOY'){
 		node{
-			sh 'echo PRE-PROD deployment started'
-
-			config.each { echo "[INFO] KRUSH $it.key : $it.value" }
-
-			environment = config.environment
 
 			playbook = 'deployment/playbook/playbook.yml'
-			inventory = "deployment/inventory/${environment}/inventory.yml"
+			inventory = "deployment/inventory/${config.environment}/inventory.yml"
 
 			ansiblePlaybook inventory: inventory, playbook: playbook
 		}
