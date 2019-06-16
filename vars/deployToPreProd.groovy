882e161e19e6acfc94f9bba1af53c14893e1f06c
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-16 02:15:01 +0100

    Second cut: Ansible colorized output enabled

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -3,7 +3,7 @@ def call(Map config){
 	stage('PRE-PROD DEPLOY'){
 		node{
 			sh 'echo PRE-PROD deployment started'
-			ansiblePlaybook inventory: 'deployment/inventory/inventory.yml', playbook: 'deployment/playbook/playbook.yml' colorized: true
+			ansiblePlaybook inventory: 'deployment/inventory/inventory.yml', playbook: 'deployment/playbook/playbook.yml', colorized: true
 		}
 	}
 	cucumber config
