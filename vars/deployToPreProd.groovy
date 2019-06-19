250f8034358a2d7c74444b005ae0931609bb33d5
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-19 18:59:13 +0100

    Ansible: inventory path changed

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -4,7 +4,7 @@ def call(Map config){
 		node{
 
 			playbook = 'deployment/playbook/playbook.yml'
-			inventory = "deployment/inventory/${config.environment}/inventory.yml"
+			inventory = "deployment/inventory/${config.environment}"
 
 			
 			ansiblePlaybook inventory: inventory, playbook: playbook
