889d9599a2d552e34f5b60379e7bdeb4ded9f084
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-27 21:42:31 +0100

    cucumber building once again

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -7,7 +7,7 @@ def call(Map config){
 			inventory = "deployment/inventory/${config.environment}"
 
 			
-			ansiblePlaybook inventory: inventory, playbook: playbook
+			ansiblePlaybook inventory: inventory, playbook: playbook -vvvv
 		}
 	}
 	cucumber config
