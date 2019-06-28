c86870b8eeb489c56dac30c9c53f8c1b17518fb8
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-06-28 06:55:56 +0100

    Debug mode removed

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -7,7 +7,7 @@ def call(Map config){
 			inventory = "deployment/inventory/${config.environment}"
 
 			
-			ansiblePlaybook inventory: inventory, playbook: playbook, extras: '-vvv'
+			ansiblePlaybook inventory: inventory, playbook: playbook
 		}
 	}
 	cucumber config
