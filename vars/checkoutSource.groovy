f0572d6a74a2afccca7cd65f3a959ac2898bac16
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-03 23:45:49 +0100

    Print all env's

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -9,6 +9,10 @@ def call(Map config){
 				log.info "$it.key  : $it.value" 
 			}
 			echo 'Pulling...' + env.BRANCH_NAME
+			log.info '====================== env ======================='
+			env.each{ 
+				log.info "$it.key  : $it.value" 
+			}
 
 			git url: "git@github.com:krushnatkhawale/student-service.git"
 		}	
