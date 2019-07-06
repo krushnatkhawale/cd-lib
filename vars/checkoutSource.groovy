782197cba9361573a5372b3cb186046c6bb90fcb
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-06 13:41:50 +0100

    Reverted build skip, no hardcoding for repo name, refactoring

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -2,25 +2,13 @@ def call(Map config){
 
 	stage("CHECKOUT"){
 		node{
-			log.info '===================== printenv ======================'
 			sh 'printenv'
-			log.info '====================== config ======================='
-			config.each{ 
-				log.info "$it.key  : $it.value" 
-			}
-			echo 'Pulling...' + env.BRANCH_NAME
-			log.info '====================== env ======================='
-			env.properties.each{ 
-				log.info "$it.key  : $it.value" 
-			}
+			config.each{ log.info "config: $it.key  : $it.value" }
+			env.properties.each{ log.info "env: $it.key  : $it.value" }
 
-			log.info '====================== CHECKOUT ======================='
 			def repositoryToCheckout = scm.userRemoteConfigs[0].getUrl()
-
 			git url: repositoryToCheckout
-			
 		}
 	}
-
-//	build config
+	build config
 }
