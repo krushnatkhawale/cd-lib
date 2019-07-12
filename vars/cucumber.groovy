e9a47530941a4331321fb215dcf3fff1be0da30c
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-12 23:24:57 +0100

    Retrieving branch name from config and passing to build task for qa

--- vars/cucumber.groovy
+++ vars/cucumber.groovy
@@ -3,7 +3,7 @@ def call(Map config){
 	stage('CUCUMBER'){
 		node{
 			sh 'echo Cucumber will be added'
-			sh './gradlew :qa:build'
+			sh './gradlew :qa:build -PtestEnvironment=$config.BRNACH_NAME'
 		}
 	}
 }
