490b515b6da403cae0b09818bf98d61bde00dcc2
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-12 23:18:19 +0100

    branch name from env

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -3,6 +3,8 @@ def call(Map config){
 	stage("CHECKOUT"){
 		node{
 			sh 'printenv'
+
+			log.info "just print: $env.BRANCH_NAME"			
 			config.each{ log.info "config: $it.key  : $it.value" }
 			env.properties.each{ log.info "env: $it.key  : $it.value" }
 
