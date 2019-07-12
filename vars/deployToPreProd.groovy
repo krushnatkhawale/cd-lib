6fc6ac5481fac67951892fe788f3d7c9c6e4303f
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-12 23:22:22 +0100

    Branch name added to config to pass to cucumber task

--- vars/deployToPreProd.groovy
+++ vars/deployToPreProd.groovy
@@ -5,8 +5,8 @@ def call(Map config){
 
 			playbook = 'deployment/playbook/playbook.yml'
 			inventory = "deployment/inventory/${config.environment}"
-
 			
+			config.BRNACH_NAME = "$env.BRANCH_NAME"
 			ansiblePlaybook inventory: inventory, playbook: playbook
 		}
 	}
