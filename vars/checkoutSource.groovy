92b1ef67e8f405fbb5348447d929ed949e23a6c4
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-13 23:31:52 +0100

    Branch name pull successful

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -3,15 +3,11 @@ def call(Map config){
 	stage("CHECKOUT"){
 		node{
 			sh 'printenv'
-
 			config.each{ log.info "config: $it.key  : $it.value" }
 			env.properties.each{ log.info "env: $it.key  : $it.value" }
 
 			def repositoryToCheckout = scm.userRemoteConfigs[0].getUrl()
-			config.BRNACH_NAME = "$env.BRANCH_NAME"
-
-			log.info "CHECKING OUT1: $repositoryToCheckout, branch: $config.BRANCH_NAME"
-			log.info "CHECKING OUT2: $repositoryToCheckout, branch: $env.BRANCH_NAME"
+			
 			git url: repositoryToCheckout, branch: env.BRANCH_NAME
 		}
 	}
