5f7e9f3359a86354b199b59823eded3547911271
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 09:09:12 +0100

    Pipeline type identification: testing branch

--- vars/pipelineChooser.groovy
+++ vars/pipelineChooser.groovy
@@ -1,6 +1,7 @@
 def call(Map config){
 
-
+	var branchName = env.getProperties().environment.BRANCH_NAME
+	log.info "Pipeline triggered for branch '$branchName'"
 	println "Test****************"
 	print env.getProperties().environment.BRANCH_NAME
 	println "Finish****************"
