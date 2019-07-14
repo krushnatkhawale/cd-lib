2c2d0a149a33b4278c03afb000cab2a339ff10c9
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 08:58:02 +0100

    Pipeline type identification: testing branch

--- vars/pipelineChooser.groovy
+++ vars/pipelineChooser.groovy
@@ -2,6 +2,7 @@ def call(Map config){
 
 
 	println "Test****************"
+	log.info environment.BRANCH_NAME
 	log.info env.getProperties().toString()
 	println "Finish****************"
 	//checkoutSource config
