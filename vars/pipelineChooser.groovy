410194b139de5576fb4bef43b4797a89ddbc348e
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 09:18:03 +0100

    Pipeline type identification: testing branch

--- vars/pipelineChooser.groovy
+++ vars/pipelineChooser.groovy
@@ -2,6 +2,25 @@ def call(Map config){
 
 	def branchName = env.getProperties().environment.BRANCH_NAME
 	log.info "Pipeline triggered for branch '$branchName'"
+	
+	switch(branchName) {
+		case ~/^master.*/:
+		log.info "Executing pipeline type -PRE-PROD"
+			
+		break
+		case ~/^develop.*/:
+		log.info "Executing pipeline type PROD"
+			
+		break
+		case ~/^feature.*/:
+		log.info "Executing pipeline type PRE-PROD"
+			
+		break
+		default:
+			
+		break
+	}
+
 	println "Test****************"
 	print env.getProperties().environment.BRANCH_NAME
 	println "Finish****************"
