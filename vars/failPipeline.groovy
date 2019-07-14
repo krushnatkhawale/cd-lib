0547b52a4b63e8dabe42265e51e5ecaa79b4b3f4
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 10:24:12 +0100

    Pipeline type identification: separate functions added

--- vars/failPipeline.groovy
+++ vars/failPipeline.groovy
@@ -1,8 +1,8 @@
 def call(Map config){
 
 	log.info '''
-Branch naming convention isn't followed, please use valid name.
-Branch name must follow following pattern, feature-XXXXXXXX'''
+		Branch naming convention isn't followed, please use valid name.
+		Branch name must follow following pattern, feature-XXXXXXXX'''
 	currentBuild.result = "ERROR"
 
 }
