8bd8a9a00f81ee503822b930661a5ea6c560c939
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 11:10:21 +0100

    Enabled preprod pipeline: log test

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,6 +2,11 @@ def call(Map config){
 
 	stage('BUILD'){
 		node{
+
+			config.each{ log.info "config: $it.key  : $it.value" }
+			env.properties.each{ log.info "env: $it.key  : $it.value" }
+
+
 			sh 'chmod +x gradlew'
 			sh './gradlew :app:build'
 			config.environment = 'dev'
--- vars/cucumber.groovy
+++ vars/cucumber.groovy
@@ -2,6 +2,11 @@ def call(Map config){
 
 	stage('CUCUMBER'){
 		node{
+
+			config.each{ log.info "config: $it.key  : $it.value" }
+			env.properties.each{ log.info "env: $it.key  : $it.value" }
+
+
 			sh 'echo Cucumber will be added'
 			sh "./gradlew :qa:build -PtestEnvironment=" + config.branchName
 		}
