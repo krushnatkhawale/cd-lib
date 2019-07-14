980b177317434177a21f9eb2896530b0d6174d69
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-14 08:41:38 +0100

    Pipeline type identification: testing branch

--- vars/cucumber.groovy
+++ vars/cucumber.groovy
@@ -3,7 +3,7 @@ def call(Map config){
 	stage('CUCUMBER'){
 		node{
 			sh 'echo Cucumber will be added'
-			sh "./gradlew :qa:build -PtestEnvironment=$env.BRNACH_NAME"
+			sh "./gradlew :qa:build -PtestEnvironment=" + $env.BRNACH_NAME
 		}
 	}
 }
--- vars/pipelineChooser.groovy
+++ vars/pipelineChooser.groovy
@@ -1,5 +1,13 @@
 def call(Map config){
 
+
+	getBranchName
+
 	checkoutSource config
 
+}
+
+def getBranchName(){
+	println "Branch: $env.BRNACH_NAME"
+	print "PIPELINE TYPE: " 
 }
