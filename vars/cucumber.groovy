5038de5ba32756e2117d0465a3b64958fe3bae83
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-15 23:58:12 +0100

    Branch specific env test

--- vars/cucumber.groovy
+++ vars/cucumber.groovy
@@ -3,9 +3,6 @@ def call(Map config){
 	stage('CUCUMBER'){
 		node{
 
-			config.each{ log.info "config: $it.key  : $it.value" }
-			env.properties.each{ log.info "env: $it.key  : $it.value" }
-
 			sh "./gradlew :qa:build -PtestEnvironment=" + config.environment
 		}
 	}
--- vars/pipelineChooser.groovy
+++ vars/pipelineChooser.groovy
@@ -3,14 +3,14 @@ def call(Map config){
 	config.branchName = env.getProperties().environment.BRANCH_NAME
 	
 	switch(config.branchName) {
+
+		case ~/^feature.*/: 
 		case ~/^master.*/:  runPreProdPipeline config
 							break
 
 		case ~/^develop.*/: runProdPipeline config
 							break
 
-		case ~/^feature.*/: runPreProdPipeline config
-							break
 		default: 			failPipeline config
 							break
 	}
