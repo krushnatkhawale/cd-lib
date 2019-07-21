6a0e636713f43edb0a78becf68b6947cdd37ca22
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-21 01:39:26 +0100

    Develop flow: first cut

---
+++ vars/runPreProdDevelopPipeline.groovy
@@ -0,0 +1,35 @@
+def call(Map config){
+
+	log.info "Pipeline triggered from branch '$config.branchName', type PRE-PROD"
+
+	config.environment = pipelineUtils.getEnvironmentFor config
+			
+
+	stage("CHECKOUT"){
+		node{
+
+			checkoutSource config			
+		}
+	}
+
+	stage('BUILD'){
+		node{
+
+			build config
+		}
+	}
+
+	stage("DEPLOY TO ${config.environment.toUpperCase()}"){
+		node{
+
+			deploy config
+		}
+	}
+
+	stage('PERFORMANCE TESTS'){
+		node{
+
+			//cucumber config
+		}
+	}
+}
