950702781c02a86914e55e2385e277b468ba2de8
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-07-31 22:48:11 +0100

    Input requested test commented out

--- vars/runPreProdDevelopPipeline.groovy
+++ vars/runPreProdDevelopPipeline.groovy
@@ -8,13 +8,17 @@ def call(Map config){
 	stage("CHECKOUT"){
 		node{
 
+
+			/*
+			
 			def userInput = input(
 			 id: 'userInput', message: 'Let\'s promote?', parameters: [
 			 [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env'],
 			 [$class: 'TextParameterDefinition', defaultValue: 'uat1', description: 'Target', name: 'target']
 			])
 			echo ("Env: "+userInput['env'])
-			echo ("Target: "+userInput['target'])
+			echo ("Target: "+userInput['target']) 
+			*/
 
 			checkoutSource config			
 		}
